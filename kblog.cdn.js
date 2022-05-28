0c32c93261319254b43b76a750aa2eecfe81b001
kuingsamlee <samleeforme@gmail.com>
2022-05-28 12:09:36 +0800

    javascript tools formatter

--- kblog.cdn.js
+++ kblog.cdn.js
@@ -1,69 +1,72 @@
 /*
- * kblog使用cdn加载js失败则加载本地文件
+ * kblog先使用cdn加载js失败则加载本地文件
  * date: 2017-02-05
- * by kuing
+ * by 0x00t0x7f
  */
+
 //单独加载 jquery文件
-function jqueryCdn(filelist){
-	var _ret =  ((function  loadfile(num){
-		var script = document.createElement("script");
-		script.src = filelist[num];
-		script.onload = function(){
-			document.body.appendChild(script);
-		}
-		script.onerror = function(){
-			num += 1;
-			if(num < filelist.length) loadfile(num);
-		}
-		script.onload();
-		return true;
-	})(0))? true:false;   //函数执行完毕 返回true
-	return _ret;
+function jqueryCdn(filelist) {
+    var _ret = ((function loadfile(num) {
+        var script = document.createElement("script");
+        script.src = filelist[num];
+        script.onload = function () {
+            document.body.appendChild(script);
+        }
+        script.onerror = function () {
+            num += 1;
+            if (num < filelist.length) loadfile(num);
+        }
+        script.onload();
+        return true;
+    })(0)) ? true : false;   //函数执行完毕 返回true
+    return _ret;
 }
 
-function jsAndcssLoad(filelist,callback){
-	//对于依赖于 jquery 的js先行判断
-	if(typeof callback == 'function'){
-		var flag = callback(["http://cdn.bootcss.com/jquery/2.1.1/jquery.min.js",
-        "js/jquery-2.1.1.min.js"
+function jsAndcssLoad(filelist, callback) {
+    //对于依赖于 jquery 的js先行判断
+    if (typeof callback == 'function') {
+        var flag = callback(["http://cdn.bootcss.com/jquery/2.1.1/jquery.min.js",
+            "js/jquery-2.1.1.min.js"
         ]);
-	}
-	if(!window.jQuery){
-		//var flag = jqueryCdn(["http://cdn.bootcss.com/jquery/2.1.1/jquery.min.js",
+    }
+    if (!window.jQuery) {
+        //var flag = jqueryCdn(["http://cdn.bootcss.com/jquery/2.1.1/jquery.min.js",
         //"js/jquery-2.1.1.min.js"
         //]);
-        var stop = setInterval(function(){
-        	if(window.jQuery) {
-        		//console.log("Jquery 已经加载");
-        		clearInterval(stop);
-        		//jquery加载完毕 执行后续 cdn js文件加载
-        		for(var i in filelist){
-					var jsfilelist = filelist[i];
-					(function loadFile(num){
-						var script = document.createElement("script");
-						script.src = jsfilelist[num];
-						//var _off = false;
-						script.onload = function(e){
-							//console.log("加载 远程 cdn文件");
-							document.body.appendChild(script);
-							//document.head.write(unescape('%3Cscript src='+filelist[num]+'%3E%3C/script%3E'));
-						}
-						script.onerror = function(){
-							//console.log("加载本地 js 文件");
-							num += 1;
-							if(num < filelist.length) loadFile(num);
-						}
-						script.onload();
-					})(0);
-				}
-        		return true;}
-        },50);//时间间隔50ms确认一次
-		if(!flag) return false; //不加载任何 cdn 文件跳出
-	}	
+        var stop = setInterval(function () {
+            if (window.jQuery) {
+                //console.log("Jquery 已经加载");
+                clearInterval(stop);
+                //jquery加载完毕 执行后续 cdn js文件加载
+                for (var i in filelist) {
+                    var jsfilelist = filelist[i];
+                    (function loadFile(num) {
+                        var script = document.createElement("script");
+                        script.src = jsfilelist[num];
+                        //var _off = false;
+                        script.onload = function (e) {
+                            //console.log("加载 远程 cdn文件");
+                            document.body.appendChild(script);
+                            //document.head.write(unescape('%3Cscript src='+filelist[num]+'%3E%3C/script%3E'));
+                        }
+                        script.onerror = function () {
+                            //console.log("加载本地 js 文件");
+                            num += 1;
+                            if (num < filelist.length) loadFile(num);
+                        }
+                        script.onload();
+                    })(0);
+                }
+                return true;
+            }
+        }, 50);//时间间隔50ms确认一次
+        if (!flag) return false; //不加载任何 cdn 文件跳出
+    }
 }
 
 //调用 参数是需要加载的文件的列表,每个列表包含一份cdn文件和本地文件
 jsAndcssLoad([
-	["http://cdn.bootcss.com/bootstrap/3.3.0/js/bootstrap.min.js",
-	"js/bootstrap.min.js"]
-],jqueryCdn);
+    [
+        "http://cdn.bootcss.com/bootstrap/3.3.0/js/bootstrap.min.js",
+        "js/bootstrap.min.js"]
+], jqueryCdn);
--- kblogImageEffectShow.js
+++ kblogImageEffectShow.js
@@ -2,87 +2,99 @@
 * 加载主页巨幕图片轮播-动态加载效果代码全部动态生成(包含css,js)
 * 建议图片大小： 1400*406(400)/幅*3
 * arguments列表:图片列表,效果-启动淡入淡出效果(依赖于jquery),速度(毫秒),开启nav导航栏透明化.
-* by kuing
+* by 0x00t0x7f
 * date: 2017-02-06
 */
-function loadImageShowScreen(imglist,position,effect,speed,navopacity){
-	if(!document.getElementById || !document.getElementsByTagName || !document.createElement) return false;
-	if(document.getElementsByTagName("nav").length == 0){
-		return false;
-	}else{
-		if(0>speed && 10<speed){
-			alert("速率参数合法值应该在0~10范围之内,请重新设置");
-			return false;
-		}
-		var showeffect = effect;
-		//此插件如果启动效果需依赖jquery
-		if(!window.jQuery && showeffect){
-			showeffect = false;
-		}
-		var nav = document.getElementsByTagName("nav")[0];
-		//document.body.appendChild(nav);
-		if(navopacity){
-		  document.getElementsByTagName("nav")[0].setAttribute("style","background-color:transparent");
-		  document.getElementById("aboutme").getElementsByTagName("span")[0].setAttribute("style","color:#20D8BE");
-		  //默认释放导航栏
-		  window.onload = function(){
-		     document.getElementById("queryinput").setAttribute("style","border-color:transparent;width:280px;margin-left:50px;border-radius:3px 0px 0px 3px;");
-		     document.getElementsByClassName("glyphicon-magnet")[0].click();
-		     document.getElementsByClassName("glyphicon-magnet")[0].className = "glyphicon glyphicon-paperclip";
-		}
-		}
-		var div = document.createElement("div");
-		div.setAttribute("id","showscreenimg");
-		div.setAttribute("class","hidden-xs hidden-sm");
-		//div.setAttribute("class","container-fluid");
-		div.setAttribute("style","width:100%;margin-top:60px");
-		//div.setAttribute("class","hidden-xs hidden-sm");
-		var img = document.createElement("img");
-		var _imagelist = imglist;
-		img.setAttribute("src",_imagelist[_imagelist.length-1]);
-		img.setAttribute("alt","巨幕图片展示");
-		//img.setAttribute("style","width: 100%;height: 860px;");
-		img.setAttribute("style","width: 100%;height: 406px;");
-		div.appendChild(img);
-  		//可选-插入位置
-  		if(position == "before"){
-		   nav.parentNode.insertBefore(div,nav);
- 		}
- 		if(position == "after"){
-		   insertAfter(div,nav);
-		}
-		//开始节点&图片库数量
-		var start = 0;
-		var _num = _imagelist.length;
-		//设置速度和循环轮播
-		setInterval(function(){
-			start = (start/_num == 1)? 0:start;
-			//console.log("正在展示第 "+(start+1)+" 张图片");
-			var img = document.getElementById("showscreenimg");
-			if(showeffect){
-				$("div#showscreenimg>img").fadeOut(200,function(){
-					img.getElementsByTagName("img")[0].setAttribute("src",_imagelist[start]);
-					start += 1;
-					$(this).fadeIn(300);
-				});
-			}else{
-				img.getElementsByTagName("img")[0].setAttribute("src",_imagelist[start]);
-				start += 1;
-			}
-		},speed*1000);
-	}
-	
-}
-function insertAfter(newElement,targetElement){
-	var target = targetElement.parentNode;
-	if(target.lastChild == targetElement){
-		target.appendChild(newElement)
-	}else{
-		target.insertBefore(newElement,targetElement.nextSibling);
-	}
+function loadImageShowScreen(imglist, position, effect, speed, navopacity) {
+    if (!document.getElementById || !document.getElementsByTagName || !document.createElement) return false;
+    if (document.getElementsByTagName("nav").length == 0) {
+        return false;
+    } else {
+        if (0 > speed && 10 < speed) {
+            alert("速率参数合法值应该在0~10范围之内,请重新设置");
+            return false;
+        }
+        var showeffect = effect;
+        //此插件如果启动效果需依赖jquery
+        if (!window.jQuery && showeffect) {
+            showeffect = false;
+        }
+        var nav = document.getElementsByTagName("nav")[0];
+        //document.body.appendChild(nav);
+        if (navopacity) {
+            document.getElementsByTagName("nav")[0].setAttribute("style", "background-color:transparent");
+            document.getElementById("aboutme").getElementsByTagName("span")[0].setAttribute("style", "color:#20D8BE");
+            //默认释放导航栏
+            window.onload = function () {
+                document.getElementById("queryinput").setAttribute("style", "border-color:transparent;width:280px;margin-left:50px;border-radius:3px 0px 0px 3px;");
+                document.getElementsByClassName("glyphicon-magnet")[0].click();
+                document.getElementsByClassName("glyphicon-magnet")[0].className = "glyphicon glyphicon-paperclip";
+            }
+        }
+        var div = document.createElement("div");
+        div.setAttribute("id", "showscreenimg");
+        div.setAttribute("class", "hidden-xs hidden-sm");
+        //div.setAttribute("class","container-fluid");
+        div.setAttribute("style", "width:100%;margin-top:60px");
+        //div.setAttribute("class","hidden-xs hidden-sm");
+        var img = document.createElement("img");
+        var _imagelist = imglist;
+        img.setAttribute("src", _imagelist[_imagelist.length - 1]);
+        img.setAttribute("alt", "巨幕图片展示");
+        //img.setAttribute("style","width: 100%;height: 860px;");
+        img.setAttribute("style", "width: 100%;height: 406px;");
+        div.appendChild(img);
+        //可选-插入位置
+        if (position == "before") {
+            nav.parentNode.insertBefore(div, nav);
+        }
+        if (position == "after") {
+            insertAfter(div, nav);
+        }
+        //开始节点&图片库数量
+        var start = 0;
+        var _num = _imagelist.length;
+        //设置速度和循环轮播
+        setInterval(function () {
+            start = (start / _num == 1) ? 0 : start;
+            //console.log("正在展示第 "+(start+1)+" 张图片");
+            var img = document.getElementById("showscreenimg");
+            if (showeffect) {
+                $("div#showscreenimg>img").fadeOut(200, function () {
+                    img.getElementsByTagName("img")[0].setAttribute("src", _imagelist[start]);
+                    start += 1;
+                    $(this).fadeIn(300);
+                });
+            } else {
+                img.getElementsByTagName("img")[0].setAttribute("src", _imagelist[start]);
+                start += 1;
+            }
+        }, speed * 1000);
+    }
+
 }
 
-(function(){
-	if(arguments.length != 5) return false;
-	loadImageShowScreen(arguments[0],arguments[1],arguments[2],arguments[3],arguments[4]);
-})(["http://static.metastdio.com/01new.jpg","http://static.metastdio.com/02new.jpg","http://static.metastdio.com/05new.jpg"],"before",true,6,true)
+function insertAfter(newElement, targetElement) {
+    var target = targetElement.parentNode;
+    if (target.lastChild == targetElement) {
+        target.appendChild(newElement)
+    } else {
+        target.insertBefore(newElement, targetElement.nextSibling);
+    }
+}
+
+(function () {
+    if (arguments.length != 5) return false;
+    loadImageShowScreen(arguments[0], arguments[1], arguments[2], arguments[3], arguments[4]);
+})(
+    [
+    "your url or image path",
+    "your url or image path",
+    "your url or image path"
+    ],
+    "before",
+    true,
+    6,
+    true
+)
+
